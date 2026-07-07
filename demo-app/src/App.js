97f17eca61e6692e21b3aa29a872d9195114036e
kamaleswaran28 <skamaleswaran28@gmail.com>
2026-07-07 10:30:59 +0530

    excuse-app intialization

---
+++ demo-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='first-row'>
+        <button>Family</button>
+        <button>Office</button>
+        <button>Children</button>
+      </div>
+      <div className='second-row'>
+        <button>College</button>
+        <button>Party</button>
+        <button>Funny</button>
+      </div>
+      <div className='third-row'>
+        <button>Unbeliveable</button>
+        <button>Developers</button>
+        <button>Gaming</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
